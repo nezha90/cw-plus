1f69bf8c6a1b90545e22b040f7bd733d9cab6c88
nezha <nezhayoung@outlook.com>
2024-10-28 11:28:47 +0800

    fix

--- contracts/cw1-whitelist/src/common.rs
+++ contracts/cw1-whitelist/src/common.rs
@@ -8,12 +8,12 @@ pub fn create_order_id(env: Env, index: usize) -> String {
 
     // 将区块高度和发送者地址作为输入进行哈希
     hasher.update(env.block.height.to_be_bytes());
-    hasher.update(env.transaction.unwrap().index);
+    hasher.update(env.transaction.unwrap().index.to_be_bytes());
     hasher.update(index.into());
 
     let result = hasher.finalize();
 
-    result.to_string()
+    format!("{:?}", result)
 }
 
 pub fn check_deposit(info: MessageInfo, total_cost: u128) -> Result<(), ContractError> {
@@ -38,9 +38,9 @@ pub fn create_batch_order_id(env: Env) -> String {
 
     // 将区块高度和发送者地址作为输入进行哈希
     hasher.update(env.block.height.to_be_bytes());
-    hasher.update(env.transaction.unwrap().index);
+    hasher.update(env.transaction.unwrap().index.to_be_bytes());
 
     let result = hasher.finalize();
 
-    result.to_string()
+    format!("{:?}", result)
 }
