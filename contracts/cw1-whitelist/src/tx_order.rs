ef6607ca35fcfce6aad1ec4234e6dba9d7e4a382
nezha <nezhayoung@outlook.com>
2024-10-28 11:03:21 +0800

    fix

--- contracts/cw1-whitelist/src/tx_order.rs
+++ contracts/cw1-whitelist/src/tx_order.rs
@@ -105,7 +105,7 @@ pub fn end_order(
     ORDER_MAP.save(deps.storage, order_id.clone(), &order)?;
 
     // 更新资源状态为未使用
-    update_status_by_resource_map(deps, resource.get_id(), Status::Unused)?;··· ··
+    update_status_by_resource_map(deps, resource.get_id(), Status::Unused)?;
 
     // 返回响应，并发送资金
     Ok(Response::new()
