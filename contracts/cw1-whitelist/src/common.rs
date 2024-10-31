afd1f83910b3b4a541d0ce7dfd77e5962a08459c
nezha <nezhayoung@outlook.com>
2024-10-31 12:21:40 +0800

    add 总金额

--- contracts/cw1-whitelist/src/common.rs
+++ contracts/cw1-whitelist/src/common.rs
@@ -1,22 +1,8 @@
 use cosmwasm_std::{Env, Uint128, Binary, StdResult, wasm_execute, WasmMsg, Storage};
 use cw20_base::msg::ExecuteMsg;
-use sha2::{Digest, Sha256};
 
 use crate::ContractError;
 use crate::consts::{LOCKED, EARNINGS};
-
-pub fn create_order_id(env: Env, index: usize) -> String {
-    let mut hasher = Sha256::new();
-
-    // 将区块高度和发送者地址作为输入进行哈希
-    hasher.update(env.block.height.to_be_bytes());
-    hasher.update(env.transaction.unwrap().index.to_be_bytes());
-    hasher.update(index.to_be_bytes());
-
-    let result = hasher.finalize();
-
-    format!("{:?}", result)
-}
 
 pub fn send(cw20: String, contract: String, amount: Uint128, msg: Binary) -> StdResult<WasmMsg> {
     wasm_execute(
