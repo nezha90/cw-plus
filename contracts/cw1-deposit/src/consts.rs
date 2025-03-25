d9d86bdc23c83a119241b6034dae0ba9a4ab5578
nezha <nezhayoung@outlook.com>
2025-03-25 17:52:46 +0800

    fix:

--- contracts/cw1-deposit/src/consts.rs
+++ contracts/cw1-deposit/src/consts.rs
@@ -13,6 +13,5 @@ pub const DENOM: &str = "utnt4";
 pub const HOUR: u64 = 60 * 60;
 pub const DAY: u64 = HOUR * 24;
 pub const YEAR: u64 = DAY * 365;
-～
 pub const PENDING_TIME: u64 = DAY * 5;
 //pub const PENDING_TIME: u64 = 60 * 5;
